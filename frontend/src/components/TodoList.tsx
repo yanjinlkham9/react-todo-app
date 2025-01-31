65de5b6b6c41b2a4e4db614e31f41ed201cc63a3
yanjinlkham9 <bilgunn93@gmail.com>
2025-01-31 15:47:07 +0900

    bichiglel

--- frontend/src/components/TodoList.tsx
+++ frontend/src/components/TodoList.tsx
@@ -8,7 +8,7 @@ import { ReduxState, Todo } from "../../types/types";
 
 export default function TodoList() {
   // useSelector()를 통해서 store의 state 가져오기
-  let todoList = useSelector((state:ReduxState) => state.todo.list);
+  let todoList = useSelector((state: ReduxState) => state.todo.list);
   //   console.log(todoList);
 
   todoList = todoList.filter((todo: Todo) => todo.done === false);
@@ -21,23 +21,23 @@ export default function TodoList() {
 
   console.log("nextID", nextID);
 
-  const clearInput = ()=>{
-       if(inputRef.current){
+  const clearInput = () => {
+    if (inputRef.current) {
       inputRef.current.value = "";
-      inputRef.current.focus();  
+      inputRef.current.focus();
     }
-  }
+  };
   // 할 일 추가 POST /todo
   const createTodo = async () => {
-    if (inputRef.current&&inputRef.current.value.trim() !== "") {
-    // state를 변경해서 화면을 바꾸는 것
-    dispatch(create({ id: nextID, text: inputRef.current.value }));
-  }
+    if (inputRef.current && inputRef.current.value.trim() !== "") {
+      // state를 변경해서 화면을 바꾸는 것
+      dispatch(create({ id: nextID, text: inputRef.current.value }));
+    }
     // DB 정보를 바꾸기 위해서 axios 요청
     await axios.post(`${process.env.REACT_APP_API_SERVER}/todo`, {
       text: inputRef.current?.value,
     });
- 
+
     clearInput();
   };
 
@@ -50,65 +50,66 @@ export default function TodoList() {
     await axios.patch(`${process.env.REACT_APP_API_SERVER}/todo/${id}`);
   };
 
-  const enterTodo = (e:React.KeyboardEvent) => {
+  const enterTodo = (e: React.KeyboardEvent) => {
     if (e.nativeEvent.isComposing) return;
     if (e.key === "Enter") createTodo();
   };
 
   useEffect(() => {
-    if(inputRef.current) inputRef.current.focus();
+    if (inputRef.current) inputRef.current.focus();
   }, []);
 
   //todo 삭제 /todo/:todoId
-  const deleteTodo = async (todoId:number)=>{
+  const deleteTodo = async (todoId: number) => {
     await axios.delete(`${process.env.REACT_APP_API_SERVER}/todo/${todoId}`);
     dispatch(del(todoId));
-
-  }
+  };
 
   //todo 수정 -> 수정/수정 취소 /content
   const [isUpdateMode, setIsUpdateMode] = useState(false);
   const [updateId, setUpdateId] = useState(0);
-  const getTodo = (todoId: number) =>{
+  const getTodo = (todoId: number) => {
     setIsUpdateMode(true); //수정 모드로 변경
-    const [todo] = todoList.filter((to)=>to.id ===todoId); //{id, text, done}
+    const [todo] = todoList.filter((to) => to.id === todoId); //{id, text, done}
     console.log("update: ", todo);
-    if(inputRef.current) inputRef.current.value = todo.text;
+    if (inputRef.current) inputRef.current.value = todo.text;
     setUpdateId(todoId);
-  }
+  };
 
-  const cancelUpdate=()=>{
+  const cancelUpdate = () => {
     setIsUpdateMode(false);
     clearInput();
-  }
+  };
 
-  const updateTodo = async ()=>{
+  const updateTodo = async () => {
     const inputValue = inputRef.current?.value as string;
-    const res = await axios.patch(`${process.env.REACT_APP_API_SERVER}/content`, {
-      id: updateId,
-      text: inputValue,
-    });
+    const res = await axios.patch(
+      `${process.env.REACT_APP_API_SERVER}/content`,
+      {
+        id: updateId,
+        text: inputValue,
+      }
+    );
     console.log(res.data); //{isSuccess}
-    if(res.data.isSuccess){
+    if (res.data.isSuccess) {
       clearInput();
     }
-    dispatch(update(updateId, inputValue))
-  }
-
+    dispatch(update(updateId, inputValue));
+  };
 
   return (
     <section>
       <h3>할 일 목록</h3>
       <div>
         <input type="text" ref={inputRef} onKeyDown={enterTodo} />
-        {isUpdateMode?
-        (<>
-        <button onClick={updateTodo}>수정</button>  
-        <button onClick={cancelUpdate}>수정 취소</button>  
-        </>) 
-        :(<button onClick={createTodo}>추가</button>)
-      }
-      
+        {isUpdateMode ? (
+          <>
+            <button onClick={updateTodo}>수정</button>
+            <button onClick={cancelUpdate}>수정 취소</button>
+          </>
+        ) : (
+          <button onClick={createTodo}>추가</button>
+        )}
       </div>
       <ul>
         {todoList.map((todo) => {
@@ -118,11 +119,11 @@ export default function TodoList() {
                 <FontAwesomeIcon icon={faCheck} />
               </button>
               <span> {todo.text} </span>
-              <button onClick={()=> getTodo(todo.id)}> 
-                <FontAwesomeIcon icon={faPencil} /> 
-                </button>
-              <button onClick={()=>deleteTodo(todo.id)}>
-              <FontAwesomeIcon icon={faTrash} />
+              <button onClick={() => getTodo(todo.id)}>
+                <FontAwesomeIcon icon={faPencil} />
+              </button>
+              <button onClick={() => deleteTodo(todo.id)}>
+                <FontAwesomeIcon icon={faTrash} />
               </button>
             </li>
           );
